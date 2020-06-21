7309ed3a8cfa3912c0bc068b59f2b35cba64fade
Joachim Holwech <joachim.holwech@gmail.com>
2020-06-21 12:30:06 +0200

    Minor fix to get frontend working with API changes

--- src/layouts/Editor.vue
+++ src/layouts/Editor.vue
@@ -49,6 +49,7 @@
         v-b-modal.modal-save-dialog
         variant="light"
         class="editor-button"
+        @click="setRecording"
       >
         Save
       </b-button>
@@ -229,166 +230,9 @@ export default class Editor extends Vue {
     }
   }
 
-  // @Prop(String) readonly id?: string;
-
-  // private RecordStore = RecordStore;
-  // private state = new AppState();
-  // private drawer = false;
-  // private container?: ServiceBuilder;
-  // private controller?: Service = undefined;
-  // private loading = false;
-  // private entries: any = {};
-  // private timer = new Timer();
-  // private dialog = false;
-  // private recordColor = 'grey';
-  // private recording = false;
-  // private playing = false;
-  // private saveDialog = false;
-  // private recordingData: IAction[] = RecordStore.recording;
-  // private msg: string = 'Drawing board';
-  // private smoothness = { text: '4 - Sharp curves', value: 4 };
-  // private slider = 0;
-  // private color = { text: 'Black', value: 'black' };
-  // private width = { text: '1px', value: 1 };
-  // private fillColor = { text: 'Black', value: 'black' };
-  // private panMode: string = 'off';
-  // private selectSmoothnessItems = [
-  //   { text: '1 - No smoothing', value: 1 },
-  //   { text: '4 - Sharp curves', value: 4 },
-  //   { text: '20 - Hyper smooth curves', value: 20 }
-  // ];
-  // private selectColorItems = [
-  //   { text: 'Black', value: 'black' },
-  //   { text: 'Blue', value: 'blue' },
-  //   { text: 'Red', value: 'red' },
-  //   { text: 'Green', value: 'green' }
-  // ];
-  // private selectWidthItems = [
-  //   { text: '1px', value: 1 },
-  //   { text: '2px', value: 2 },
-  //   { text: '4px', value: 4 },
-  //   { text: '8px', value: 8 }
-  // ];
-  // private fill = false;
-  // private helpText = [
-  //   'Click play/space to start recording',
-  //   'Click reverse and then play to view recording',
-  //   'Hold CTRL and click left mouse button to pan',
-  //   'Scroll to zoom',
-  //   'Click on drawing to remove it'
-  // ];
-
-  // private saveDialogOpen(dialog: boolean) {
-  //   this.saveDialog = dialog;
-  // }
-
-  // private playToggle(e: KeyboardEvent): void {
-  //   if (this.saveDialog) {
-  //     return;
-  //   }
-  //   if (e.keyCode === 32 || e.key === ' ') {
-  //     if (this.isPlaying) {
-  //       this.controller!.pause();
-  //     } else {
-  //       this.controller!.start();
-  //     }
-  //   }
-  // }
-
-  // private panOn(e: KeyboardEvent): void {
-  //   if (e.keyCode === 17) {
-  //     this.panMode = 'on';
-  //     this.controller!.stateToggle(true);
-  //   }
-  // }
-
-  // private panOff(e: KeyboardEvent): void {
-  //   if (e.keyCode === 17) {
-  //     this.panMode = 'off';
-  //     this.controller!.stateToggle(false);
-  //   }
-  // }
-
-  // private async mounted() {
-  //   this.container = new ServiceBuilder();
-  //   this.controller = this.container.build(
-  //     document.getElementById('svg')!,
-  //     this.state,
-  //     this.timer
-  //   );
-  //   if (this.id) {
-  //     let player = this.container
-  //       .getContainer()
-  //       .resolve<PlayBaseController>(PlayBaseController);
-  //     await Promise.all([
-  //       RecordStore.LoadMetadata({ id: this.id, auth: this.$auth }),
-  //       RecordStore.LoadRecording({ id: this.id, auth: this.$auth })
-  //     ]);
-  //     console.log(RecordStore.recording);
-  //     player.setEventLog(RecordStore.recording);
-  //   } else {
-  //     this.controller.init([
-  //       { targetAttr: StrokeAttributes.COLOR, value: this.color.value },
-  //       { targetAttr: StrokeAttributes.WIDTH, value: this.width.value },
-  //       {
-  //         targetAttr: StrokeAttributes.BUFFER_SIZE,
-  //         value: this.smoothness.value
-  //       },
-  //       { targetAttr: StrokeAttributes.FILL, value: undefined }
-  //     ]);
-  //   }
-  //   window.addEventListener('keydown', this.panOn);
-  //   window.addEventListener('keyup', this.panOff);
-  //   window.addEventListener('keydown', this.playToggle);
-  // }
-
-  // private clear(): void {
-  //   this.controller!.clear();
-  // }
-
-  // private setStrokeProperties(attr: StrokeAttributes): void {
-  //   let value;
-  //   switch (attr) {
-  //     case StrokeAttributes.COLOR:
-  //       value = this.color.value;
-  //       break;
-  //     case StrokeAttributes.WIDTH:
-  //       value = this.width.value;
-  //       break;
-  //     case StrokeAttributes.BUFFER_SIZE:
-  //       value = this.smoothness.value;
-  //       break;
-  //     case StrokeAttributes.FILL:
-  //       value = this.fill ? this.fillColor.value : undefined;
-  //       break;
-  //     default:
-  //       break;
-  //   }
-  //   this.controller!.setStrokeProperties({
-  //     targetAttr: attr,
-  //     value
-  //   });
-  // }
-
-  // get isPlaying(): boolean {
-  //   return this.state.state === AppStates.START;
-  // }
-
-  // private save(): void {
-  //   RecordStore.SetRecording(this.controller!.export());
-  //   this.dialog = true;
-  // }
-
-  // private printAccessToken(): void {
-  //   this.$auth
-  //     .getAccessTokenAsync({
-  //       scopes: [
-  //         process.env.VUE_APP_SCOPE_WRITE,
-  //         process.env.VUE_APP_SCOPE_READ
-  //       ]
-  //     })
-  //     .then(el => console.log(el.accessToken));
-  // }
+  private setRecording(): void {
+    RecordStore.SetRecording(this.board.controller!.export())
+  }
 }
 </script>
 
